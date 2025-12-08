817dc51e4b1cc1ce99b08fde5f305ab04c35570b
Bilal Sadiq <bnaq860@gmail.com>
2025-12-08 14:00:14 +0530

    Added Model Training Module

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ sys.path.append("src")
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionStageTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelStageTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -24,6 +27,18 @@ try:
         logger.info(f">>>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<")
         prepare_base_model = PrepareBaseModelStageTrainingPipeline()
         prepare_base_model.main()
+        logger.info(f">>>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Training Stage"
+try:
+        logger.info(f">>>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<")
+        model_training = ModelTrainingPipeline()
+        model_training.main()
         logger.info(f">>>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
